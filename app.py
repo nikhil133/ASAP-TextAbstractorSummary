e9547664c5ac5f7d4980a238442ab47a5287b971
nik133 <nnikhilvasudev@gmail.com>
2020-07-29 13:32:23 +0530

    jsonified the api response

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from predict import predict
 app=Flask(__name__)
 
 @app.route("/app/v1/incoming")
 def GetText():
     text=request.args.get('text')
-    print(text)
-    return predict(text)
+    print("Request for ",text)
+    body={
+        'success':True,
+        'summary':predict(text)
+    }
+    return jsonify(body)
 
 if __name__=='__main__':
     app.run(debug=True)
